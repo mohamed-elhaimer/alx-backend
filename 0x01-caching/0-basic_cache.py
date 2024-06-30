0ff4a7563a2d44629450fbd15c3c77750c2113ea
Logic <mohamedelheimer0@gmail.com>
2024-06-30 20:10:42 +0100

    all

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Basic dictionary"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """caching system"""
+    def __init__(self):
+        """constructor"""
+        super().__init__()
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data"""
+        if key or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
