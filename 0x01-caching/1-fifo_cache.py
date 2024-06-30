8768b74794ef08b8f4a5b9c066c8f1a9ada878ae
Logic <mohamedelheimer0@gmail.com>
2024-06-30 22:11:13 +0100

    all

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""FIFO caching"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """IFO Cache Algorithme"""
+    def __init__(self):
+        """constructor"""
+        super().__init__()
+
+    def put(self, key, item):
+        """put in the cache"""
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key = next(iter(self.cache_data))
+            del self.cache_data[first_key]
+            print(f'DISCARD: {first_key}')
+
+    def get(self, key):
+        """get item from cache by key"""
+        return self.cache_data.get(key)
