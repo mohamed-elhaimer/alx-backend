95c6041843c43bc46d2dfdd148ffdcc5fcf47951
Logic <mohamedelheimer0@gmail.com>
2024-07-11 17:37:46 +0100

    all

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python3
+"""A Basic Flask app.
 """
-0. Basic Flask app
-"""
-
-from flask import Flask, render_template, request
 from flask_babel import Babel
+from flask import Flask, render_template
 
 
 class Config:
-    """ class that has a LANGUAGES class attribute """
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-    LANGUAGES = ["en", "fr"]
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['BABEL_DEFAULT_LOCALE'] = Config.BABEL_DEFAULT_LOCALE
-app.config['BABEL_DEFAULT_TIMEZONE'] = Config.BABEL_DEFAULT_TIMEZONE
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @app.route('/')
-def index() -> str:
-    """ Route app """
+def get_index() -> str:
+    """The home/index page.
+    """
     return render_template('1-index.html')
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
