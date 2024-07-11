7c294668efc6f0116073c3853cf3adc3c1e422d2
Logic <mohamedelheimer0@gmail.com>
2024-07-11 17:23:33 +0100

    all

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+0. Basic Flask app
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ class that has a LANGUAGES class attribute """
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.config['BABEL_DEFAULT_LOCALE'] = Config.BABEL_DEFAULT_LOCALE
+app.config['BABEL_DEFAULT_TIMEZONE'] = Config.BABEL_DEFAULT_TIMEZONE
+babel = Babel(app)
+
+
+@app.route('/')
+def index() -> str:
+    """ Route app """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+0. Basic Flask app
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ class that has a LANGUAGES class attribute """
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.config['BABEL_DEFAULT_LOCALE'] = Config.BABEL_DEFAULT_LOCALE
+app.config['BABEL_DEFAULT_TIMEZONE'] = Config.BABEL_DEFAULT_TIMEZONE
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ select the langueage based on the header http """
+
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/')
+def index() -> str:
+    """ Route app """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
